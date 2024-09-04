518fdbbdf45ae5ca93411f6ca7c72971e652ff29
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2024-09-04 20:08:41 +0859

    chore: udpate unit test

--- src/util.test.ts
+++ src/util.test.ts
@@ -3,5 +3,7 @@ import { substitute } from "./util";
 test("substitute", () => {
   process.env.TestEnv = "/tmp";
   const path_result = substitute<string>("${env:TestEnv}/bin/neocmakelsp");
-  assert.equal(path_result, "/tmp/bin/neocmakelsp")
+  assert.equal(path_result, "/tmp/bin/neocmakelsp");
+  const path_result_normal = substitute<string>("/usr/bin/neocmakelsp");
+  assert.equal(path_result_normal, "/usr/bin/neocmakelsp")
 })
